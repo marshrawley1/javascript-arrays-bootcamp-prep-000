585ee358bbe8af94d810551a52a2499381f191de
marshrawley1 <marshallrawley@gmail.com>
2017-11-16 17:41:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(chocolateBars, 'twix'){
+/*function addElementToBeginningOfArray(chocolateBars, 'twix'){
 ['twix', ...chocolateBars]
 return chocolateBars
 }
 
-/*function destructivelyAddElementToBeginningOfArray(chocolateBars, 'twix'){
+function destructivelyAddElementToBeginningOfArray(chocolateBars, 'twix'){
 chocolateBars.unshift('twix')
 return chocolateBars
 }
