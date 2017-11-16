9147f55ce15eeff564c7033ea3506688e2deca22
marshrawley1 <marshallrawley@gmail.com>
2017-11-16 17:33:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(chocolateBars, 'twix'){
+['twix', ...chocolateBars]
+return chocolateBars
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, 'twix'){
+chocolateBars.unshift('twix')
+return chocolateBars
+}
+
+function addElementToEndOfArray(chocolateBars, 'twix'){
+[...chocolateBars, 'twix']
+return  chocolateBars
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, 'twix'){
+chocolateBars.push('twix')
+return chocolateBars
+}
