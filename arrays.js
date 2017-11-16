79f0380b2fad9eab30dcdcbdefae36b8ded55bcf
marshrawley1 <marshallrawley@gmail.com>
2017-11-16 17:40:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-/*function addElementToBeginningOfArray(chocolateBars, 'twix'){
+function addElementToBeginningOfArray(chocolateBars, 'twix'){
 ['twix', ...chocolateBars]
 return chocolateBars
 }
